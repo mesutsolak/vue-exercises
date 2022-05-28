2bbaded588ed4c29f75fe732845ebda0602b49e4
Mesut <mesuttsolakk@gmail.com>
2022-05-28 20:17:54 +0300

    Adding and deleting operations are done in the user management application.

--- applications/usermanagement/usermanagement.js
+++ applications/usermanagement/usermanagement.js
@@ -34,16 +34,21 @@ const userApp = new Vue({
         this.alerttype = "alert alert-danger";
         this.alerttext = "Lütfen boş bıraktığınız yerleri doldurun.";
       } else if (
-        this.users.some((user) => user.email == this.activeUser.email)
+        this.users.some((user) => user.email == this.activeUser.email) &&
+        this.users.some((user) => user.id == this.activeUser.id)
       ) {
         this.alerttype = "alert alert-danger";
         this.alerttext = "Email zaten kayıtlı.";
       } else {
-        this.activeUser.id = Math.max.apply(
-          Math,
-          this.users.map((user) => user.id)
-        ) + 1;
-        this.users.push(this.activeUser);
+        if (!this.users.some((user) => user.id == this.activeUser.id)) {
+          this.activeUser.id =
+            Math.max.apply(
+              Math,
+              this.users.map((user) => user.id)
+            ) + 1;
+          this.users.push(this.activeUser);
+        }
+
         this.alerttype = "alert alert-success";
         this.alerttext = "Kullanıcı başarıyla kaydedildi.";
         this.clearUser();
@@ -56,6 +61,22 @@ const userApp = new Vue({
       this.clearUser();
       this.alerttype = "";
       this.alerttext = "";
+    },
+    removeUserIndex(index) {
+      this.users.splice(index, 1);
+      //Array içinden bir vei silmek için ikinci yöntem.
+      //this.$delete(this.users,index);
+    },
+    removeUserId(id) {
+      var foundUser = this.users.find((user) => user.id == id);
+      if (foundUser != null) {
+        this.users = this.users.filter((user) => user.id != foundUser.id);
+      }
+    },
+    userFind(id) {
+      debugger;
+      $('#newUserModal').modal("show");
+      this.activeUser = this.users.find((user) => user.id == id);
     },
   },
 });
