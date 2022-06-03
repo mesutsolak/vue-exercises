a7285ba50f3250b5954d804dc09dc83a13c47cf3
Mesut <mesuttsolakk@gmail.com>
2022-06-03 13:39:14 +0300

    Component usage has been realized in the CLI project.

--- topics/vue-cli/src/App.vue
+++ topics/vue-cli/src/App.vue
@@ -1,22 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <hello-world msg="Welcome to Your Vue.js App" />
+    <alert-count></alert-count>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import AlertCount from "./components/AlertCount.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    "hello-world": HelloWorld,
+    "alert-count": AlertCount,
+  },
+};
 </script>
 
 <style>
+/*
+    Style içerisinde lang diye tabir ettiğimiz özellik stil işlemlerinin derleneceği yapıyı belirtmektedir.
+    Örneğin : lang = "scss" yazdığımızda scss derleme özelliğini kullanacağını bilgisini vermiş oluyoruz.
+    scoped ise tanımladığımız css bilgilerinin sadece bu component içinde kullanılacağı anlammına gelmektedir.
+    Zaten bu komponentlerin ayrılmasının birinci sebebi kendi özelinde kodların yazılmasıdır.
+    Global olarak tanımlamak istediklerimizi App.Vue içerisinde yapmamız gerekmektedir.
+    App.Vue bilindiği gibi başlangıç dosyamızdır.Main.js içerisinden ayarlıyoruz.
+    Komponentleri ayırt etmek için div içinde id değeride verebilirsiniz.
+    <template> içerisinde html tasarımlarımızı yapabiliriz.
+    export default içerisinde components ve props tanımlayabilir bunları gerekli gördüğümüz yerde kullanabiliriz.
+    Komponent içinde "hello-world" ya da helloWorld: olarak tanımlayıp <template> içerisinde kullandıktan
+    sonra çıkan hata otomatik olarak düzelecektir.
+    - karakteri varsa  ' ' içerisinde yazmamız gerekir.
+ */
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
