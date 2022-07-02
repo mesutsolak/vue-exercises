82cd8081858e2ae3adc5410690c83c25b8c7efc4
Mesut <mesuttsolakk@gmail.com>
2022-07-03 00:24:07 +0300

    Mutation usage learned

--- topics/vue-cli/src/components/CardPage.vue
+++ topics/vue-cli/src/components/CardPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="card" style="width:18rem;">
+    <div class="card" style="width:18rem;" v-if="profileImage">
     <img class="card-img-top" :src="profileImage" alt="Profil Resmi">
     <div class="card-body">
         <h5 class="card-title">
@@ -10,6 +10,12 @@
         <p class="card-text"> {{ $store.state.activeUser.Title }} </p>
         <p class="card-text"> {{ $store.state.activeUser.LikeNumber }} beğenisi var</p>
     </div>
+
+    <hr>
+    <button @click="UserSettings" class="btn btn-primary mr-2">Kullanıcı Ayarla</button>
+    <button @click="increaseLikes" class="btn btn-success mr-1">Beğeni Arttır</button>
+    <button @click="decreaseLikes" class="btn btn-danger">Beğeni Azalt</button>
+
   </div>
 </template>
 
@@ -22,10 +28,29 @@ import { mapGetters } from 'vuex'
     Vuex içerisinde sadece resim ismini tutmalıyız.
     ... spead operatörü computed içerisinde hem method , property kullanımına izin 
     verirken hem de mapgetters kullanımına izin vermektedir.
+    Mutation metotlar senkron olarak çalışması zorunludur.Asenkron bir işlem yapamamaktayız.
+    Asenkron olarak kullanmak için Actionlardan yararlanmaktayız.
+    Google chrome extension vue kullanarak her bir mutation işlemini görebilir ve revert
+    diyerek geri alma işlemini yapabiliriz.
+
+    Revert - Time Travel - Commit
+    Commit : ilgili mutation artık base state hale gelmiştir.Bundan sonraki mutation işlemleri
+    bu mutation işlemi bu base state üzerinden ilerlemiş olacaktır.
+    Time Travel : bir önceki duruma geçebiliriz.
+    Revert All : Hepsini geriye al.
 */ 
 
 export default {
     name:'app',
+    // created(){
+    //     // local yerine global olarak tanımlanmalıdır.
+    //     this.$store.state.activeUser.LikeNumber = 100;
+    // },
+    created(){
+        // this.$store.mutations.UserSetting(); diyemiyoruz.
+        //commit içine mutation ismini yazarak çağırma işlemimizi gerçekleştiriyoruz.
+        this.$store.commit('UserSetting');
+    },
     computed:{
         // computed:mapGetters(['isPopular','firstAndLastName'])
         // firstAndLastName(){
@@ -36,9 +61,28 @@ export default {
         //     return this.$store.getters.isPopular;
         // }
         profileImage(){
+        /*
+            Sayfa ilk açıldığında resim yüklenmediği için değerin var olup olmadığının
+            kontrolünün yapılması gerekmektedir.
+        */ 
+            if(this.$store.state.activeUser.profileImage){
             return require('../assets/' + this.$store.activeUser.profileImage);
+            }
+return null;
         },
         ...mapGetters(['isPopular','firstAndLastName'])
+    },
+    methods:{
+        userSetting(){
+            this.$store.commit('userSetting');
+        },
+        increaseLikes(){
+            this.$store.commit('increaseLikes',5);
+        },
+        decreaseLikes(){
+            //obje gönderebiliriz
+            this.$store.commit('decreaseLikes',{ value : 1 });
+        }
     }
 };
 </script>
--- topics/vue-cli/src/store/index.js
+++ topics/vue-cli/src/store/index.js
@@ -10,15 +10,38 @@ export default new Vuex.Store({
       lastName: "Solak",
       Title: "Yazılım Geliştirme Uzmanı",
       likeNumber: 25,
-      profilImage: "profile-mesut.jpg",
+      profilImage: "profile-mesut.jpg", // Yol değişebilir canlı ve prod ortamda bu yüzden sadece isim tutulmalıdır.
     },
   },
   getters: {
-    //Var olan state (veri) özelleştirerek tekrardan veri şeklinde kullanmamıza yaramaktadır.
+    /*
+      Var olan state (veri) özelleştirerek tekrardan veri şeklinde kullanmamıza yaramaktadır.
+      State parametresi sayesinde vuex içinde tanımlı olan değerlere erişebiliriz.
+    */
     firstAndLastName: (state) =>
       state.activeUser.firstName + " " + state.activeUser.lastName,
     ispopular: (state) => state.activeUser.likeNumber > 20,
   },
-  mutations: {},
+  mutations: {
+    userSetting(state) {
+      state.activeUser = {
+        firstName: "Mesut",
+        lastName: "Solak",
+        Title: "Yazılım Geliştirme Uzmanı",
+        likeNumber: 25,
+        profilImage: "profile-mesut.jpg",
+      };
+    },
+    increaseLikes(state, n) {
+      /*
+      n kadar arttır diyerek istediğimiz sayı kadar arttırabiliriz.
+      n değerine payload diyoruz.
+      */
+      state.activeUser.likeNumber += n;
+    },
+    decreaseLikes(state, payload) {
+      state.activeUser.likeNumber += payload.value;
+    },
+  },
   actions: {},
 });
