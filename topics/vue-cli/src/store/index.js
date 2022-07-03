b909fd4f7cc73b90e1a0a8622b0faaa1510a6782
Mesut <mesuttsolakk@gmail.com>
2022-07-03 13:44:02 +0300

    Module topic learned.

--- topics/vue-cli/src/store/index.js
+++ topics/vue-cli/src/store/index.js
@@ -3,6 +3,88 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+/*
+  Store yapıları büyüdükçe yönetilmeleri zorlaşmaktadır.Bunun için store yapılarını
+  parçalayıp modüller altına toplamamız hem daha düzgün hem de yönetimi daha kolay olacaktır.
+  view ->  {{ $store.state.userModule.activeUser.Title }}
+  kod -> return this.$store.state.userModule.activeUser.Title;
+*/
+
+
+// let userStore = {
+//   state: {
+//     activeUser: {
+//       firstName: "Mesut",
+//       lastName: "Solak",
+//       Title: "Yazılım Geliştirme Uzmanı",
+//       likeNumber: 25,
+//       profilImage: "profile-mesut.jpg", // Yol değişebilir canlı ve prod ortamda bu yüzden sadece isim tutulmalıdır.
+//     },
+//   },
+//   getters: {
+//     /*
+//       Var olan state (veri) özelleştirerek tekrardan veri şeklinde kullanmamıza yaramaktadır.
+//       State parametresi sayesinde vuex içinde tanımlı olan değerlere erişebiliriz.
+//     */
+//     firstAndLastName: (state) =>
+//       state.activeUser.firstName + " " + state.activeUser.lastName,
+//     ispopular: (state) => state.activeUser.likeNumber > 20,
+//   },
+//   mutations: {
+//     userSetting(state) {
+//       state.activeUser = {
+//         firstName: "Mesut",
+//         lastName: "Solak",
+//         Title: "Yazılım Geliştirme Uzmanı",
+//         likeNumber: 25,
+//         profilImage: "profile-mesut.jpg",
+//       };
+//     },
+//     increaseLikes(state, n) {
+//       /*
+//       n kadar arttır diyerek istediğimiz sayı kadar arttırabiliriz.
+//       n değerine payload diyoruz.
+//       */
+//       state.activeUser.likeNumber += n;
+//     },
+//     decreaseLikes(state, payload) {
+//       state.activeUser.likeNumber += payload.value;
+//     },
+//   },
+//   actions: {
+//     // Parametre olarak context değerini almaktadır.
+//     UserSetting(context) {
+//       context.commit("UserSetting");
+//     },
+//     // context içindeki commit değerine direk olarak ulaşabiliriz.State değerinide alabilmekteyiz.
+//     // Commitler mutations metotları çağırmaktadır.Mutation tek işlem yapabilirken actionlar asenkron çalıştığı için birden fazla işlem yapabilmektedir.
+//     increaseLikes: ({ commit }, payload) => commit("imcreaseLikes", payload),
+//     decreaseLikes: ({ commit, state }) => {
+//       if (state.activeUser.likeNumber > 0) commit("decreaseLikes");
+//     },
+//     increaseLikesAsync: ({ commit }, payload) => {
+//       //resolve,reject parametrelerini almaktadır.
+//       return new Promise((resolve) => {
+//         setTimeout(() => {
+//           commit("imcreaseLikes", payload);
+//           resolve(); // çağırma işlemi yaptıktan sonra tamamlanınca .then() içine yazdığımız kodlar çalışır.
+//         }, 3000);
+//       });
+//     },
+//   },
+// };
+
+//Modul tanımı
+
+// export default new Vuex.Store({
+//   modules :{
+//     userModule : userStore
+//   }
+// });
+
+
+
+
 export default new Vuex.Store({
   state: {
     activeUser: {
@@ -65,3 +147,4 @@ export default new Vuex.Store({
     },
   },
 });
+
