6a8d1ea0921ad1acfd90f47ddba77bc2fbc8d799
Mesut <mesuttsolakk@gmail.com>
2022-07-08 19:30:37 +0300

    The first introduction to the Interceptor structure has been made.

--- topics/vue-cli/src/assets/js/apiService.js
+++ topics/vue-cli/src/assets/js/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default axios.create({
+let instance = axios.create({
   baseUrl: "http://127.0.0.1:8000/api/",
   timeout: 500,
   headers: {
@@ -8,3 +8,29 @@ export default axios.create({
     "Content-type": "application/json",
   },
 });
+
+/* 
+  Interceptor yapılarını farklı axios instancelarında kullanabilirsiniz.İnterceptor yapıları
+  request ve response ifadelerini yönetmemize sağlayan bir yapıdır.
+  Console.log kullanarak bize gelen data içerisindeki verileri daha iyi bir şekilde görebiliriz.
+
+  vue.interceptors.request.use -> şeklinde kullanım yaparsak bu düz bir şekilde axios kullanan yerleri etkileyecektri.Özel olarak
+  create edilen axios yapısını etkilemeyecektir.
+*/
+
+instance.interceptors.request.use(function (config) {
+  console.log(
+    "Request yapıldı (" + config.method + ")",
+    config.baseUrl + config.url
+  );
+
+  return config;
+});
+
+instance.interceptors.response.use(function (response) {
+  console.log("Response yapıldı (" + response.status + ")");
+
+  return response;
+});
+
+export default instance;
