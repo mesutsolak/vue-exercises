dd86682d5a2e6e1abf176cb8a06b72703fc2d8a5
Mesut <mesuttsolakk@gmail.com>
2022-07-09 13:01:58 +0300

    Preparation was made before publishing.

--- topics/vue-cli/vue.config.js
+++ topics/vue-cli/vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  // Projelerimizi ana dizinde değil de vue-proje gibi bir alt klasörde yayınlamak için alttaki yapıyı kullanırız.
+  baseUrl: process.env.NODE_ENV === "production" ? "/vue-proje" : "/",
+});
