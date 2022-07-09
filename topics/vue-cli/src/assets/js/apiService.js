b2194bc07459102fd9e1001c8495d08ba9b6d022
Mesut <mesuttsolakk@gmail.com>
2022-07-09 11:02:42 +0300

    Global error catching with Interceptor

--- topics/vue-cli/src/assets/js/apiService.js
+++ topics/vue-cli/src/assets/js/apiService.js
@@ -16,6 +16,9 @@ let instance = axios.create({
 
   vue.interceptors.request.use -> şeklinde kullanım yaparsak bu düz bir şekilde axios kullanan yerleri etkileyecektri.Özel olarak
   create edilen axios yapısını etkilemeyecektir.
+
+ 
+
 */
 
 instance.interceptors.request.use(function (config) {
@@ -27,10 +30,34 @@ instance.interceptors.request.use(function (config) {
   return config;
 });
 
-instance.interceptors.response.use(function (response) {
-  console.log("Response yapıldı (" + response.status + ")");
+instance.interceptors.response.use(
+  //Dönen response değeri başarılı ise
+  function (response) {
+    console.log("Response yapıldı (" + response.status + ")");
 
-  return response;
-});
+    return response;
+  },
+  function (error) {
+    // Dönen dizi değerini parçalamak için
+    const { status, statusText, data } = error.response;
+
+    // if (error.response != null) {
+    //   toastr.error(error.response.data.message, "Hata");
+    // } else {
+    //   toastr.error(error.message, "Hata");
+    // }
+
+    if (status >= 500) {
+      toastr.error(data.message, "Hata");
+    }
+
+    /* 
+      Çağrılan yerde .then() kullanırken blok içerisine girmesin diye kullanmaktayız.
+      if(response && response.data) -> Bu bloğu yazmaya gerek kalmayacaktır.
+
+    */
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
--- topics/vue-cli/src/assets/js/userService.js
+++ topics/vue-cli/src/assets/js/userService.js
@@ -7,7 +7,12 @@ export default {
     return apiService.get(resource, { params: { page } });
   },
   GetById(id) {
-    return apiService.get(resource + "/" + id);
+    return apiService.get(resource + "/" + id).then((response) => {
+      //response undefined gelebilir
+      if (response && response.data) {
+        return response;
+      }
+    });
   },
   DeleteById(id) {
     return apiService.delete(resource + "/" + id);
